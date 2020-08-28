8e89b611bbf530a136b4a47f951ddb3856a70b38
Bruno Silva <55957337+brunofpsilva@users.noreply.github.com>
2020-08-28 02:26:01 +0100

    20. Delete item from UI

--- starter/app.js
+++ starter/app.js
@@ -167,6 +167,11 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newhtml);
         },
 
+        deleteListItem: function (selectorID) {
+            var element = document.getElementById(selectorID);
+            element.parentNode.removeChild(element);
+        },
+
         clearFields: function () {
             var fields, fieldsArray;
 
@@ -268,9 +273,10 @@ var controller = (function (budgetCtrl, UICtrl) {
             budgetCtrl.deleteItem(type, ID);
 
             // 2. Delete the item from the UI
-
+            UICtrl.deleteListItem(itemID);
 
             // 3. Update and show the new budget
+            updateBudget();
         }
     }
 
