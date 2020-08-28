486fbc3f8126b881c3fd08561ddcb9a2ed94bc04
Bruno Silva <55957337+brunofpsilva@users.noreply.github.com>
2020-08-28 01:08:53 +0100

    13. Updating the budget Controller Parte 3(Display on UI)

--- starter/app.js
+++ starter/app.js
@@ -99,7 +99,11 @@ var UIController = (function () {
         inputvalue: '.add__value',
         inputBtn: '.add__btn',
         incomeContainer: '.income__list',
-        expenseContainer: '.expenses__list'
+        expenseContainer: '.expenses__list',
+        budgetLabel: '.budget__value',
+        incomeLabel: '.budget__income--value',
+        expenseLabel: '.budget__expenses--value',
+        percentageLabel: '.budget__expenses--percentage'
     };
 
     return {
@@ -161,6 +165,21 @@ var UIController = (function () {
             fieldsArray[0].focus();
         },
 
+        displayBudget: function (obj) {
+
+            document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+            document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+            document.querySelector(DOMstrings.expenseLabel).textContent = obj.totalExp;
+
+            if (obj.percentage > 0) {
+                document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage;
+            }
+            else {
+                document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+            }
+
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -191,7 +210,7 @@ var controller = (function (budgetCtrl, UICtrl) {
         budget = budgetCtrl.getBudget();
 
         // 3. Update the budget on the UI
-        console.log(budget);
+        UICtrl.displayBudget(budget);
     }
 
     var ctrlAddItem = function () {
@@ -220,6 +239,12 @@ var controller = (function (budgetCtrl, UICtrl) {
         init: function () {
             console.log("Application has started.");
             setupEventListeners();
+            UICtrl.displayBudget({
+                budget: 0,
+                totalInc: 0,
+                totalExp: 0,
+                percentage: -1
+            });
         }
     }
 
